9242ed978581d0fd4d319bc92aef81ccb4824fd8
Eugene Mirotin <emirotin@gmail.com>
2011-08-08 13:28:58 +0300

    strict comparison

--- js/core.js
+++ js/core.js
@@ -119,7 +119,7 @@ $(document).ready(function () {
         $.each(values, function() {
             var properties = this.split("=");
 
-            if ((typeof properties[0] != 'undefined') && (typeof properties[1] != 'undefined')) {
+            if ((typeof properties[0] !== 'undefined') && (typeof properties[1] !== 'undefined')) {
                 serialized_values[properties[0].replace(/\+/g, " ")] = properties[1].replace(/\+/g, " ");
             }
         });
@@ -129,7 +129,7 @@ $(document).ready(function () {
         $(this).find(":input").removeAttr('checked').each(function() {
             var tag_name = $(this).attr("name");
             if (values[tag_name] !== undefined) {
-                if ($(this).attr("type") == "checkbox") {
+                if ($(this).attr("type") === "checkbox") {
                     $(this).attr("checked", "checked");
                 } else {
                     $(this).val(values[tag_name]);
